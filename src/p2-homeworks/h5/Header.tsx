bb0545a12d38b2288912afcbe9ebd7750df410ba
YauheniZhukovets <92811758+YauheniZhukovets@users.noreply.github.com>
2021-12-14 15:04:52 +0300

    5HW

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -5,7 +5,9 @@ import {PATH} from './Rout';
 function Header() {
     return (
         <div>
-            <NavLink to={PATH.PRE_JUNIOR}>pre-junior</NavLink>
+            <NavLink to={PATH.PRE_JUNIOR}>per-junior</NavLink>
+            <NavLink to={PATH.JUNIOR}>junior</NavLink>
+            <NavLink to={PATH.JUNIOR_PLUS}>JuniorPlus</NavLink>
         </div>
     )
 }
