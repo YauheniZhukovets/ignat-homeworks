0c3e699b606af1c214f6538571a3490b9f860f79
YauheniZhukovets <92811758+YauheniZhukovets@users.noreply.github.com>
2021-12-17 16:48:06 +0300

    5HW

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -6,7 +6,7 @@ import s from './Header.module.css'
 function Header() {
     return (
         <div className={s.header}>
-            <NavLink to={PATH.PRE_JUNIOR} className={({isActive})=> isActive ? s.activeLink : s.defLink}>per-junior</NavLink>
+            <NavLink to={PATH.PRE_JUNIOR} className={({isActive})=> isActive ? s.activeLink : s.defLink}>pre-junior</NavLink>
             <NavLink to={PATH.JUNIOR} className={({isActive})=> isActive ? s.activeLink : s.defLink}>junior</NavLink>
             <NavLink to={PATH.JUNIOR_PLUS} className={({isActive})=> isActive ? s.activeLink : s.defLink}>JuniorPlus</NavLink>
             <img className={s.picBlock}
